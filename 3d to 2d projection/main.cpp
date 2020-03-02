0812d6f32b92b1bf52e72b7912e81907102da411
szebestik@gmail.com <szebestik@gmail.com>
2020-03-02 20:41:02 +0100

    basic window

--- 3d to 2d projection/main.cpp
+++ 3d to 2d projection/main.cpp
@@ -1,8 +1,11 @@
 #include <SFML/Graphics.hpp>
 
+const int width = 800;
+const int height = 600;
+
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "3d to 2d");
+	sf::RenderWindow window(sf::VideoMode(width, height), "3d to 2d");
 
 	while (window.isOpen())
 	{
@@ -13,6 +16,11 @@ int main()
 				window.close();
 		}
 	}
+
+	window.clear();
+
+
+	window.display();
 
 	return 0;
 }
