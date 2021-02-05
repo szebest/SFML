028e156baa88779acbc4d7b2ee7ade62456e971a
szebestik@gmail.com <szebestik@gmail.com>
2021-02-05 17:42:39 +0100

    Created functions to rotate and draw a line. Created a recursive function to draw a tree with some parameters

--- Recursive Tree/main.cpp
+++ Recursive Tree/main.cpp
@@ -5,12 +5,61 @@
 #include <cstdlib>
 #include <iostream>
 
+#define M_PI 3.14159265358979323846
+
 #define WIDTH 1600
 #define HEIGHT 900
 
 #define FRAMETIME 16666
 
 using namespace std;
+
+void draw(float len, float angle, sf::Vector2f& point, sf::RenderWindow& window) {
+	//calculate the offset between the first and second point
+	float x = cos(angle * M_PI / 180.f) * len;
+	float y = sin(angle * M_PI / 180.f) * len;
+	//apply the position of the first point
+	sf::Vertex linia2[2];
+	linia2[0].position = point;
+	linia2[0].color = sf::Color::White;
+	//calculate the position of the second point
+	point = { point.x + x, point.y + y };
+	//apply the position of the second point
+	linia2[1].position = point;
+	linia2[1].color = sf::Color::White;
+	//draw the line
+	window.draw(linia2, 2, sf::Lines);
+}
+
+void rotate(float& angle, float rotation) {
+	//apply the rotation
+	angle += rotation;
+	//Keep it in the range from -360 to 360 degrees
+	if (angle >= 360)
+		angle -= 360;
+	if (angle <= -360)
+		angle += 360;
+}
+
+void drawTree(float length, float mini, float angle, float rotation, sf::Vector2f startingPosition, sf::RenderWindow& window) {
+	//exit condtion
+	if (length < mini) 
+		return;
+	else {
+		//draw the line
+		draw(length, angle, startingPosition, window);
+		//rotate
+		rotate(angle, rotation);
+		//recursion
+		drawTree(length / 2, mini, angle, rotation, startingPosition, window);
+		//rotate 2 times in the other direction
+		rotate(angle, -2*rotation);
+		//recursion
+		drawTree(length / 2, mini, angle, rotation, startingPosition, window);
+		//rotate back
+		rotate(angle, rotation);
+	}
+}
 
 int main()
 {
@@ -31,6 +80,8 @@ int main()
             zegar.restart();
 
             window.clear();
+
+			drawTree(300, 5, -90, 25, {WIDTH / 2, HEIGHT * 0.8},  window);
 
             window.display();
 
